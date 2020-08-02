290f79160278ab58f7ab37d8a878c974a2bc7922
Mubbashir Ali <mubbashirali35@gmail.com>
2020-08-02 05:34:12 +0500

    Todo Mongoose model updated 1

--- models/todos.js
+++ models/todos.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const Todo = mongoose.Schema({
+    title: {
+        type: String,
+        default: 'Untitled Todo'
+    },
     description: {
         type: String,
         required: true
